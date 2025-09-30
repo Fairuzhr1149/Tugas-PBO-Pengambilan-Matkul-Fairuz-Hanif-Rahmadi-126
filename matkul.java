fc22b49b633c16b4a4c072aef7deffa48b1af20d
Fairuzhr1149 <rahmadifairuzhanif@gmail.com>
2025-09-30 21:41:57 +0700

    Update matkul.java

--- matkul.java
+++ matkul.java
@@ -1,52 +1,39 @@
-package pengambilan;
-
 import java.util.ArrayList;
 
 public class matkul {
 
-	private String nama;
-	public dosen pengajar;
-	private String jam;
-	private int sks;
-	public static ArrayList<Mahasigma> mahasiswa = new ArrayList();
-	
-	
-	public matkul(String nama, dosen pengajar, String jam, int sks, ArrayList<Mahasigma> mahasiswa) {
-		this.nama = nama;
-		this.pengajar = pengajar;
-		this.jam = jam;
-		this.sks = sks;
-		this.mahasiswa = mahasiswa;
-	}
-	
-	
-	
-	
-	
-	ArrayList<matkul> contoh = new ArrayList();
-	
+    private String nama;
+    public dosen pengajar;
+    private String jam;
+    private int sks;
+    private ArrayList<Mahasigma> mahasiswa; // HAPUS static
 
-	public String getmtkNama() {
+    public matkul(String nama, dosen pengajar, String jam, int sks, ArrayList<Mahasigma> mahasiswa) {
+        this.nama = nama;
+        this.pengajar = pengajar;
+        this.jam = jam;
+        this.sks = sks;
+        this.mahasiswa = mahasiswa;
+    }
+
+    public String getmtkNama() {
         return nama;
     }
-	
-	
-	public ArrayList<Mahasigma> getmhs(){
-		return mahasiswa;
-	}
-	
-	public void addMahasiswa(Mahasigma m) {
-	    mahasiswa.add(m);
-	}
 
-	public void showMahasiswa() {
-	    System.out.println("Mahasiswa yang mengambil " + nama + ":");
-	    for (Mahasigma m : mahasiswa) {
-	        System.out.println("- " + m.namamhs + " (" + m.nrp + ")");
-	    }
-	}
-	
+    public ArrayList<Mahasigma> getmhs(){
+        return mahasiswa;
+    }
 
-	
-	
+    public void addMahasiswa(Mahasigma m) {
+        mahasiswa.add(m);
+    }
+
+    public void showMahasiswa() {
+        System.out.println("Mahasiswa yang mengambil " + nama + ":");
+        for (Mahasigma m : mahasiswa) {
+            System.out.println("- " + m.namamhs + " (" + m.nrp + ")");
+        }
+    }
 }
+
+
