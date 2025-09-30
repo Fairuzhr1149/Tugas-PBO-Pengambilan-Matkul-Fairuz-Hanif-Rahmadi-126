d162d5b75e169c82c7d693c87e4085dfc1bf1df9
Fairuzhr1149 <rahmadifairuzhanif@gmail.com>
2025-09-30 21:45:39 +0700

    Update Mahasigma.java

--- Mahasigma.java
+++ Mahasigma.java
@@ -1,29 +1,27 @@
-package pengambilan;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mahasigma {
 
-	public String namamhs;
-	public String nrp;
-	private String passmhs;
-	
-	public Mahasigma(String namamhs, String nrp, String passmhs) {
-		this.namamhs = namamhs;
-		this.nrp = nrp;
-		this.passmhs = passmhs;
-	}
-	
-	public static ArrayList<Mahasigma> mhs = new ArrayList<>();
-	ArrayList <matkul> jadwal = new ArrayList <matkul>();
-	static Scanner scan = new Scanner(System.in);
-	
-	public static void logscrmhs() {
+    public String namamhs;
+    public String nrp;
+    private String passmhs;
+    public ArrayList<matkul> jadwal = new ArrayList<>(); // jadwal tiap mahasiswa
+
+    public Mahasigma(String namamhs, String nrp, String passmhs) {
+        this.namamhs = namamhs;
+        this.nrp = nrp;
+        this.passmhs = passmhs;
+    }
+
+    public static ArrayList<Mahasigma> mhs = new ArrayList<>();
+    static Scanner scan = new Scanner(System.in);
+
+    public static void logscrmhs() {
         System.out.println("1. login");
         System.out.println("2. sign up");
         int loginmhs = scan.nextInt();
-        scan.nextLine(); 
+        scan.nextLine();
 
         if (loginmhs == 1) {
             loginmhs();
@@ -59,7 +57,7 @@ public class Mahasigma {
         String newusermhs = scan.nextLine();
 
         for (Mahasigma d : mhs) {
-            if (d.mhs.equals(newusermhs)) {
+            if (d.namamhs.equals(newusermhs)) { // FIX disini
                 System.out.println("username sudah ada!");
                 return;
             }
@@ -73,13 +71,16 @@ public class Mahasigma {
 
         mhs.add(new Mahasigma(newusermhs, newnrp, newpassmhs));
         System.out.println("akun berhasil dibuat!");
-        
         loginmhs();
     }
-    
+
     public static Mahasigma getmhs(int index) {
         return mhs.get(index);
     }
-	
-	
+
+    @Override
+    public String toString() {
+        return namamhs + " (" + nrp + ")";
+    }
 }
+
